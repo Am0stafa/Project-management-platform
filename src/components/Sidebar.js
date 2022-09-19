0b8ffd9ee563982673ef86a7d657c53005340cb6
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2022-09-19 02:31:13 +0200

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,41 @@
+import { NavLink } from "react-router-dom"
+import { useAuthContext } from '../hooks/useAuthContext'
+
 import "./Sidebar.css"
+import DashboardIcon from '../assets/dashboard_icon.svg'
+import AddIcon from '../assets/add_icon.svg'
 
 export default function Sidebar() {
-
-
+  const {user}= useAuthContext()
+  
   return (
     <div className="sidebar">
-     
+      <div className="sidebar-content">
+        <div className="user">
+          <p> hello abdo</p>
+        </div>
+        
+        <nav className="links">
+          <ul>
+            <li>
+              <NavLink to="/dashboard">
+                <img src={DashboardIcon} alt="DashboardIcon"/>
+                <span>Dashboard</span>
+              </NavLink>
+            </li>
+            
+            <li>
+              <NavLink to="/create">
+                <img src={AddIcon} alt="AddIcon"/>
+                <span>New project</span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        
+        
+      </div>
     </div>
   )
 }
+//? show us the dashboard and create page
