521ec1a28249f58a52aefb6b4243f6be19243566
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2022-09-24 23:45:28 +0200

    made a filter

--- src/pages/dashboard/ProjectFilter.js
+++ src/pages/dashboard/ProjectFilter.js
@@ -1,8 +1,34 @@
+import { useState } from 'react'
+const filterList = ['all', 'mine', 'development', 'design', 'marketing', 'sales'] //^ output a button for each one
 
 const ProjectFilter = ({changeFilter}) => {
+  const [currentFilter, setCurrentFilter] = useState('all')
+
+  const handleClick = (filter) => {
+    setCurrentFilter(filter)
+  
+  }
+
   return (
-    <div></div>
+    <div className="project-filter">
+        <nav>
+          <p>Filter by: </p>
+          {
+            filterList.map((filter,id) =>(
+              <button 
+                key={id} 
+                className={currentFilter === filter ? 'active' : ''} 
+                onClick={() => handleClick(filter)}
+              >
+                {filter}
+              </button>
+            ))
+          }
+        </nav>
+    
+    </div>
   )
 }
 
 export default ProjectFilter
+//! nav with all of the different buttons we can click to add different filters
